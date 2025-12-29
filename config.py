18fd97749694224df03b489431d0a56ce483ce5c
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-12-29 15:43:44 +0100

    First set of files

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hopsworks_api_key = os.getenv("HOPSWORKS_API_KEY")
+hopsworks_project_name = os.getenv("HOPSWORKS_PROJECT_NAME")
+
+Arlanda = "ARN"
+SMHI_Weather_URL = "https://opendata-download-metfcst.smhi.se/api"
+Airport_Coordinates = {
+    "ARN": {"lat": 59.6519, "long": 17.9186},
+}
+
+
+
