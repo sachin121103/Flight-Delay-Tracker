f73673b83d1efbd74b6013c62385aecd6df64742
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2025-12-30 19:19:51 +0100

    Updated Swedavia API

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 hopsworks_api_key = os.getenv("HOPSWORKS_API_KEY")
 hopsworks_project_name = os.getenv("HOPSWORKS_PROJECT_NAME")
+swedavia_api_key = os.getenv("SWEDAVIA_API_PRIMARY_KEY")
 
 Arlanda = "ARN"
 SMHI_Weather_URL = "https://opendata-download-metfcst.smhi.se/api"
